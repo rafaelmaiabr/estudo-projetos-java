ecd49871bbef1dd217a607e4c8aea958479c4f7a
rafaelmaiabr <rafaelmaiabr@gmail.com>
2023-04-25 20:16:34 -0300

    Getters e Setters

--- POO04_ClassesEObjetos/src/controller/Cadastro.java
+++ POO04_ClassesEObjetos/src/controller/Cadastro.java
@@ -11,44 +11,53 @@ public class Cadastro {
 		
 		
 		//Instanciando Objeto
-		Carro hach = new Carro();
-		hach.fabricante = "Fiat";
-		hach.modelo = "Uno";
-		hach.cor = "Preto";
-		hach.ano = 2015;
+		Carro uno = new Carro();
 		
-		Carro sedan = new Carro();
-		sedan.fabricante = "Fiat";
-		sedan.modelo = "Linea";
-		sedan.cor = "Preto";
-		sedan.ano = 2015;
+		// inserindo dados no objeto "uno"
+		uno.setFabricante("Fiat");
+		uno.setModelo("Uno");
+		uno.setCor("Preto");
+		uno.setAno(2015);
 		
+//		Carro sedan = new Carro();
+//		sedan.fabricante = "Fiat";
+//		sedan.modelo = "Linea";
+//		sedan.cor = "Preto";
+//		sedan.ano = 2015;
+//		
 		Carro user = new Carro();
-		user.fabricante = Teclado.lerTexto("Digite o nome do Fabricante?");
-		user.modelo = Teclado.lerTexto("Agora o Modelo?");
-		user.cor = Teclado.lerTexto("QUal seria a Cor?");
-		user.ano = Teclado.lerInt("E o Ano?");
+		user.setFabricante(Teclado.lerTexto("Digite o nome do Fabricante?"));;
+//		user.modelo = Teclado.lerTexto("Agora o Modelo?");
+//		user.cor = Teclado.lerTexto("QUal seria a Cor?");
+//		user.ano = Teclado.lerInt("E o Ano?");
 		
 		System.out.println("------------------------");
-		System.out.println("Fabricante: " + hach.fabricante);
-		System.out.println("Modelo: " + hach.modelo);
-		System.out.println("Cor" + hach.cor);
-		System.out.println("Ano: " + hach.ano);
+		System.out.println("Fabricante: " + uno.getFabricante());
+		System.out.println("Modelo: " + uno.getModelo());
+		System.out.println("Cor: " + uno.getCor());
+		System.out.println("Ano: " + uno.getAno());
+		
+//		System.out.println("------------------------");
+//		System.out.println("Fabricante: " + sedan.fabricante);
+//		System.out.println("Modelo: " + sedan.modelo);
+//		System.out.println("Cor" + sedan.cor);
+//		System.out.println("Ano: " + sedan.ano);
+//		
+		System.out.println("------------------------");
+		System.out.println("Fabricante: " + user.getFabricante());
+//		System.out.println("Modelo: " + user.modelo);
+//		System.out.println("Cor: " + user.cor);
+//		System.out.println("Ano: " + user.ano);	
+		
+//		Alterando valor depois de declarado
+		
+		kombi.setCor("Preta");
 		
 		System.out.println("------------------------");
-		System.out.println("Fabricante: " + sedan.fabricante);
-		System.out.println("Modelo: " + sedan.modelo);
-		System.out.println("Cor" + sedan.cor);
-		System.out.println("Ano: " + sedan.ano);
-		
-		System.out.println("------------------------");
-		System.out.println("Fabricante: " + user.fabricante);
-		System.out.println("Modelo: " + user.modelo);
-		System.out.println("Cor" + user.cor);
-		System.out.println("Ano: " + user.ano);
-
-		
-		
+		System.out.println("Fabricante: " + kombi.getFabricante());
+		System.out.println("Modelo: " + kombi.getModelo());
+		System.out.println("Cor: " + kombi.getCor());
+		System.out.println("Ano: " + kombi.getAno());
 
 	}
 	
--- POO04_ClassesEObjetos/src/model/Carro.java
+++ POO04_ClassesEObjetos/src/model/Carro.java
@@ -2,21 +2,59 @@ package model;
 
 public class Carro {
 	
-	public String fabricante;
-	public String modelo;
-	public String cor;
-	public int ano;
+	private String fabricante;
+	private String modelo;
+	private String cor;
+	private int ano;
 	
 	// método construtor simples
 	public Carro() {
-		
+		super();
 	}
 	
+	// método construtor completo
 	public Carro(String fabricante, String modelo, String cor, int ano) {
+		super();
 		this.fabricante = fabricante;
 		this.modelo = modelo;
 		this.cor = cor;
 		this.ano = ano;
 	}
+
+	// Métodos Getters e Setters (GET e SET)
+	// Atalho Eclipse - Source > Generate Getters and Setters
+	
+	public String getFabricante() {
+		return fabricante;
+	}
+	
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public int getAno() {
+		return ano;
+	}
+	
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
 		
 }
